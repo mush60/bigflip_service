271decad4fe3dd214a36bfe4a55d4738209749fc
mush60 <mushti8@gmail.com>
2019-07-09 21:17:36 +0700

    add get_data by id

--- get_data.php
+++ get_data.php
@@ -1,11 +1,22 @@
 <?php
 
+$data = array(
+  "bank_code" => "bni",
+  "account_number" => "3233",
+  "amount" => "32322",
+  "remark" => "3232"
+);
+$payload = json_encode($data);
+
 $ch = curl_init();
 $secret_key = "HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41";
+$encsky = base64_encode($secret_key);
 
-curl_setopt($ch, CURLOPT_URL, "https://nextar.flip.id");
+curl_setopt($ch, CURLOPT_URL, "https://nextar.flip.id/disburse/5535152564");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
+// curl_setopt($ch, CURLOPT_POST, TRUE);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
   "Content-Type: application/x-www-form-urlencoded"
@@ -15,5 +26,9 @@ curl_setopt($ch, CURLOPT_USERPWD, $secret_key.":");
 
 $response = curl_exec($ch);
 curl_close($ch);
+?>
 
-var_dump($response);
+// var_dump($payload);
+<pre>
+<?php var_dump($response);?>
+</pre>
